32254ff800d0eed7f680d1106137a600ec4ff19f
Ruben Solorio <rsolm72@gmail.com>
2018-12-23 23:37:47 -0700

    Update templates.js

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -13,7 +13,7 @@ angular.module('template-store.templates',['ngRoute'])
 }])
 
 .controller('TemplatesCtrl', ['$scope','$http',function($scope, $http){
-  $http.get('/json/templates.json').success( (data) => $scope.templates=data) );
+  $http.get('/json/templates.json').success( (data) => $scope.templates=data );
 }])
 
 .controller('TemplatesDetailsCtrl', ['$scope',function($scope){
